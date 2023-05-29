43084c9a24c3fdf5d50331e13cbd0b655d92ca7a
Dony Martín Castro Jiménez <20210324@itla.edu.do>
2023-05-29 01:49:14 -0400

    Switched to N-tier architecture usage and changed the bootswatch theme.

---
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,25 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category{ get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
