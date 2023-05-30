a0aafc577ceb36288cb775118f5fa9a5aff71cc4
Dony Martín Castro Jiménez <donycastro80@gmail.com>
2023-05-29 22:10:17 -0400

    Added Product model, and its CRUD Operations, and views for each one.

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
